49e8242764460d22a00db4870fccb91c86b37c7f
sai vamsi <saivamsi.b2773@gmail.com>
2023-12-18 19:35:14 +0530

    first commit

---
+++ src/main/java/com/example/H2CurdOperations/H2CurdOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.H2CurdOperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2CurdOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2CurdOperationsApplication.class, args);
+	}
+
+}
